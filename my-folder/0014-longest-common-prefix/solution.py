1791e9b7f6b9c5393f852c84d6e88dd72c0d6b5a
Lily Shen <93230862+lilys7@users.noreply.github.com>
2026-09-26 03:46:21 +0000

    [LeetCode Sync] Runtime - 7 ms (5.56%), Memory - 12.4 MB (35.88%)

---
+++ my-folder/0014-longest-common-prefix/solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        #prefix is what the word starts with
+        res = ""
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i == len(s) or s[i] != strs[0][i]:
+                    return res
+               
+            res += s[i]
+        return res
+        
